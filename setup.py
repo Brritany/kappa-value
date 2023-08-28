59737c9eec3f0dbdafd16292d2b8a9f3b894d14b
Yong-Zhen Huang <92352655+Brritany@users.noreply.github.com>
2023-08-29 02:36:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_kappa_package',
-    version='0.1',
+    name='calculate_kappa_value',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -10,5 +10,16 @@ setup(
         'sklearn',
         'statsmodels',
         'tqdm'
+    ],
+    python_requires='>=3.6, <4',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
